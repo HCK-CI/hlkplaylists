37b4aa444f4c56f8c22bfb9958bf1237781b927e
Yan Vugenfirer <yan@daynix.com>
2018-08-27 12:33:26 +0300

    Initial commit
    Signed-off-by: Yan Vugenfirer <yan@daynix.com>

---
+++ update.sh
@@ -0,0 +1,14 @@
+rm *.xml
+wget http://aka.ms/hlkplaylist
+unzip hlkplaylist
+rm hlkplaylist
+
+mv 'HLK Version 1607 CompatPlaylist.xml'         '1607.xml'
+mv 'HLK Version 1703 CompatPlaylist.xml'         '1703.xml'
+mv 'HLK Version 1709 CompatPlaylist x86_x64.xml' '1709.xml'
+mv 'HLK Version 1803 CompatPlaylist x86_x64.xml' '1803.xml'
+
+rm 'HLK Version 1709 CompatPlaylist ARM64.xml'
+rm 'HLK Version 1709 CompatPlaylist ARM64_x86 on ARM64.xml'
+rm 'HLK Version 1803 CompatPlaylist ARM64.xml'
+rm 'HLK Version 1803 CompatPlaylist ARM64_x86 on ARM64.xml'
