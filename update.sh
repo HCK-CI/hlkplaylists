3ccc45b8e5bfa966b1473d3a829428affcc6eebc
Lior Haim <lior@daynix.com>
2018-10-03 13:45:11 +0300

    Adding comments for supported and unsupported playlists
    Signed-off-by: Lior Haim <lior@daynix.com>

--- update.sh
+++ update.sh
@@ -4,12 +4,14 @@ unzip hlkplaylist
 mv CompatPlaylists/* .
 rm -r hlkplaylist CompatPlaylists
 
+# Playlists supported by AutoHCK
 mv 'HLK Version 1607 CompatPlaylist.xml'            '1607.xml'
 mv 'HLK Version 1703 CompatPlaylist.xml'            '1703.xml'
 mv 'HLK Version 1709 CompatPlaylist x86_x64.xml'    '1709.xml'
 mv 'HLK Version 1803 CompatPlaylist x86_x64.xml'    '1803.xml'
 mv 'HLK Version 1809 CompatPlaylist x86_x64.xml'    '1809.xml'
 
+# Playlists not yet supported by AutoHCK
 rm 'HLK Version 1709 CompatPlaylist ARM64.xml'
 rm 'HLK Version 1709 CompatPlaylist ARM64_x86 on ARM64.xml'
 rm 'HLK Version 1803 CompatPlaylist ARM64.xml'
