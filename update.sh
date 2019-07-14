984c67c3021db0a0819c16675b1c16c908bacb05
Lior Haim <lior@dotcore.co.il>
2019-07-14 11:01:49 +0000

    Update playlist script to support new HLK1903

--- update.sh
+++ update.sh
@@ -10,6 +10,7 @@ mv 'HLK Version 1709 CompatPlaylist x86_x64.xml'    '1709.xml'
 mv 'HLK Version 1803 CompatPlaylist x86_x64.xml'    '1803.xml'
 mv 'HLK Version 1809 CompatPlaylist x86_x64.xml'    '1809.xml'
 mv 'HLK Version 1809 CompatPlaylist x64 Server.xml' '1809server.xml'
+mv 'HLK Version 1903 CompatPlaylist x86 x64 ARM64.xml' '1903.xml'
 
 # Playlists not yet supported by AutoHCK
 rm 'HLK Version 1709 CompatPlaylist ARM64.xml'
@@ -18,3 +19,5 @@ rm 'HLK Version 1803 CompatPlaylist ARM64.xml'
 rm 'HLK Version 1803 CompatPlaylist ARM64_x86 on ARM64.xml'
 rm 'HLK Version 1809 CompatPlaylist ARM64_x86 on ARM64.xml'
 rm 'HLK Version 1809 CompatPlaylist ARM64 .xml'
+rm 'HLK Version 1903 CompatPlaylist ARM64_x86_on_ARM64.xml'
+rm 'HLK Version 1903 CompatPlaylist ARM64 Server.xml'
